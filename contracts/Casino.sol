a2c18dd5e6eff24d8480160a96997a3d17b85b72
Taylor Lindman <37625878+TLindman@users.noreply.github.com>
2018-04-08 16:14:54 -0400

    Update Casino.sol

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.11;
 
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+///Look into this import.
 
 /// @title Contract to bet Ether for a number and win randomly when the number of bets is met.
 /// @author Merunas Grincalaitis
